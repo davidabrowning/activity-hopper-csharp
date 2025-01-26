e999ec85c8d64413f75c7ae76a46c30525dcebab
David Browning <david.a.browning@gmail.com>
2025-01-26 13:02:15 +0100

    Set up menu skeleton.

--- ActivityHopperCsharp/Program.cs
+++ ActivityHopperCsharp/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool run = true;
+            while (run)
+            {
+                ShowMenu();
+                switch (Console.ReadLine().ToUpper())
+                {
+                    case "Q":
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine("Go for a walk");
+                        break;
+                }
+            }
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("[ENTER] Choose a random activity");
+            Console.WriteLine("[Q] Quit");
         }
     }
 }
